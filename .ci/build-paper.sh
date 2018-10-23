17011a82a1d10e0c0b68c754e0bfc73d997fe54b
kedenk <denk.kevin@web.de>
2018-10-23 11:45:04 +0200

    Fix in build.sh

--- .ci/build-paper.sh
+++ .ci/build-paper.sh
@@ -22,7 +22,7 @@ then
   cd $TRAVIS_BUILD_DIR
   git checkout --orphan $TRAVIS_BRANCH-pdf
   git rm -rf .
-  git add -f paper/ms.pdf
+  git add -f paper/main.pdf
   git -c user.name='travis' -c user.email='travis' commit -m "building the paper"
   git push -q -f https://$GITHUB_USER:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG $TRAVIS_BRANCH-pdf
 fi
